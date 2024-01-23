245a24e1bea3aa8c07c84af68b651ddec0953dad
Danil Tkachenko <tkachenkodanil06@gmail.com>
2024-01-23 21:25:45 +0200

    add gulp

---
+++ gulp/config/ftp.js
@@ -0,0 +1,6 @@
+export let configFTP = {
+	host: "", // Адрес FTP сервера
+	user: "Danil Tkachenko", // Имя пользователя
+	password: "0896745231", // Пароль
+	parallel: 5 // Кол-во одновременных потоков
+}
---
+++ src/js/app.js
@@ -0,0 +1,2 @@
+import {isWebp} from "./modules/webp.js";
+isWebp();
