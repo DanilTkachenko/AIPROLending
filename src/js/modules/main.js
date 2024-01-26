5104e9311dcbd771748002e2be51582258dc0239
Maksym <mvoron@gmail.com>
2024-01-26 20:04:51 +0100

    part of product card work

--- src/js/modules/main.js
+++ src/js/modules/main.js
@@ -119,6 +119,167 @@ const swiperBenefits = new Swiper(".benefit__swiper", {
 	},
 });
 
+// <-- product-tabs logic -->
+const tabMonth = document.querySelectorAll('.product__tabs-months li');
+
+for (const months of tabMonth) {
+		months.addEventListener('click', (event) => {
+			const monthCount = event.target;
+				clearActiveClasses();
+				monthCount.classList.add('product__tab-month-active');
+		})
+}
+
+function clearActiveClasses () {
+	tabMonth.forEach((tabs) => {
+		tabs.classList.remove('product__tab-month-active')
+	})
+}
+
+// <-- product card logic -->
+
+const selectMonth = document.querySelectorAll('.product__tabs-months li');
+const price = document.querySelector('.price__currency');
+const discount = document.querySelector('.product__card-discount');
+const cardProfit = document.querySelector('.product__card-profit');
+const cardPeople = document.querySelector('.card__people-select');
+const currency = document.querySelector('.product__tabs-currency');
+
+const monthsSelect = {
+	oneMonth: {
+		currency: {
+			DOLLARS: ['$15','$20','$27'],
+			EUR: ['€14','€19','€25'],
+			UAH: ['₴567','₴756','₴1 020'],
+		},
+		discount: false,
+		price: ['$15','$20','$27'],
+		amountOfMonth: '1 month',
+		numberOfPeople: ['1-2', '3-4', '5-9'],
+	},
+
+	threeMonths: {
+		currency: {
+			DOLLARS: ['$40','$55','$71'],
+			EUR: ['€37','€50','€65'],
+			UAH: ['₴1513','₴2081','₴2686'],
+		},
+		price: ['$40','$55','$71'],
+		amountOfMonth: '3 months',
+		discount: ['-11%','-8%','-12.5%'],
+		cardProfit: `<span>$60</span> $165 for 3 months`,
+		numberOfPeople: ['1-2', '3-4', '5-9'],
+	},
+
+	sixMonths: {
+		currency: {
+			DOLLARS: ['$40','$55','$71'],
+			EUR: ['€37','€50','€65'],
+			UAH: ['₴1513','₴2081','₴2686'],
+		},
+		price: ['$75','$105','$136'],
+		amountOfMonth: '6 months',
+		discount: ['-25%','-30%','-32.5%'],
+		cardProfit: `<span>$60</span> $165 for 3 months`,
+		numberOfPeople: ['1-2', '3-4', '5-9'],
+	},
+
+	oneYear: {
+		currency: {
+			DOLLARS: ['$40','$55','$71'],
+			EUR: ['€37','€50','€65'],
+			UAH: ['₴1513','₴2081','₴2686'],
+		},
+		price: ['$100','$150','$200'],
+		discount: false,
+		amountOfMonth: '1 year',
+		cardProfit: `<span>$60</span> $165 for 3 months`,
+		numberOfPeople: ['1-2', '3-4', '5-9'],
+	}
+
+}
+
+const oneMonth = monthsSelect.oneMonth;
+const threeMonths = monthsSelect.threeMonths;
+const sixMonths = monthsSelect.sixMonths;
+const oneYear = monthsSelect.oneYear;
+
+function numberOfPeople(month) {
+	cardPeople.addEventListener('change', () => {
+		const selectedValue = cardPeople.value;
+
+		switch (selectedValue) {
+			case '1-2':
+				price.textContent = month.price[0];
+				discount.innerHTML = month.discount[0] || '';
+				break;
+
+			case '3-4':
+				price.textContent = month.price[1];
+				discount.innerHTML = month.discount[1] || '';
+				break;
+
+			case '5-9':
+				price.textContent = month.price[2];
+				discount.innerHTML = month.discount[2] || '';
+				break;
+
+			default:
+				break;
+		}
+	})
+}
+
+numberOfPeople(monthsSelect.threeMonths);
+
+function handleSelectClick(e) {
+	const selectData = e.target.innerText;
+
+	switch (selectData) {
+		case oneMonth.amountOfMonth:
+			price.textContent = oneMonth.price[1]
+			discount.innerHTML = '' ;
+			discount.classList.remove('product__card-discount') ;
+			cardProfit.innerHTML = '';
+			numberOfPeople(oneMonth);
+			break;
+
+		case threeMonths.amountOfMonth:
+			price.textContent = threeMonths.price[1]
+			discount.innerHTML = threeMonths.discount[1] ;
+			discount.classList.add('product__card-discount');
+			cardProfit.innerHTML = threeMonths.cardProfit;
+			numberOfPeople(threeMonths);
+			break;
+
+		case sixMonths.amountOfMonth:
+			price.textContent = sixMonths.price[1]
+			discount.innerHTML = sixMonths.discount[1] ;
+			discount.classList.add('product__card-discount');
+			cardProfit.innerHTML = sixMonths.cardProfit;
+			numberOfPeople(sixMonths);
+			break;
+
+		case oneYear.amountOfMonth:
+			price.textContent = oneYear.price[1]
+			discount.innerHTML = '' ;
+			discount.classList.remove('product__card-discount') ;
+			cardProfit.innerHTML = '';
+			numberOfPeople(oneYear);
+			break;
+
+			default:
+			console.log('Done!');
+			break;
+	}
+
+}
+
+for (const selected of selectMonth) {
+	selected.addEventListener('click', handleSelectClick);
+}
+
+
 // <-- flip product card -->
 const flippedButtonEl = document.querySelector(".flip-button");
 const backButtonEl = document.querySelector(".back-button");
