00f4393da4ce6078dc1f797f4cfdc9b00a4a3f91
TarasiykPavlo <kronusator909@gmail.com>
2024-01-29 18:13:19 +0200

    Flip fix second

--- src/js/modules/scroll-reveal.js
+++ src/js/modules/scroll-reveal.js
@@ -21,14 +21,14 @@ sr.reveal('.home__title', {
 sr.reveal('.home__subtitle', { delay: 600, reset:true});
 sr.reveal('.home__description', {delay: 800, reset:true});
 // sr.reveal(document.querySelector('.products__tabs'), {distance: '150%', origin: 'left', duration: 1200, reset:true});
+sr.reveal('.home__img', {rotate: {x: -50, y: 220 },  distance: '105%', origin: 'right',  duration: 2000, reset:true});
+// sr.reveal('.home__img', {interval: 900, duration: 3500, rotate: {x: 25, y: 55 }, reset: true});
+ sr.reveal('.instructions__step', {interval: 500, duration: 1500, delay: 100, reset: true});
+ sr.reveal('.benefits__container', {distance: '150%', origin: 'left', duration: 1200, reset:true});
+ sr.reveal('.for-whom__item', {interval: 600, duration: 1500, reset:true});
 
-sr.reveal('.home__img', {interval: 900, duration: 3500, rotate: {x: 25, y: 55 }});
- sr.reveal('.instructions__step', {interval: 500, duration: 1500, delay: 599, reset: true});
- sr.reveal('.benefits__container', { scale: 0.3, duration: 3000, reset:true});
- sr.reveal('.for-whom__item', {interval: 800, duration: 1500, reset:true});
-
- sr.reveal(document.querySelectorAll('.products'), {scale: 0.4, rotate: {x: -180, y: 0}, duration: 1500, reset:true});
- sr.reveal('.faq__question', {interval: 900, duration: 1200, reset:true});
+ sr.reveal(document.querySelectorAll('.products'), {scale: 1, rotate: {x: -180, y: 0}, duration: 1500, reset:true});
+ sr.reveal('.faq__question', {interval: 600, duration: 1200, reset:true});
 
 
  
