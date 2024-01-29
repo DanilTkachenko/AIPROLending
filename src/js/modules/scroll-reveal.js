3faaf745c15feccadd9e54727bed64d8d2cc7820
TarasiykPavlo <kronusator909@gmail.com>
2024-01-29 13:25:43 +0200

    Add scrollReveal

--- src/js/modules/scroll-reveal.js
+++ src/js/modules/scroll-reveal.js
@@ -1,3 +1,35 @@
+import ScrollReveal from 'scrollreveal';
+
+
 export function scrollReveal() {
-	
-}
+	const configAll = {
+        interval: 1600,
+        duration: 1500,
+        reset: true
+    };
+    
+    
+    const sr = ScrollReveal();
+    
+    
+// sr.reveal('.home__data', { interval: 1500, duration: 1500});
+sr.reveal('.home__title', {
+    scale: 3,
+    duration: 1500,
+    reset: true
+});
+sr.reveal('.home__subtitle', { delay: 600, reset:true});
+sr.reveal('.home__description', {delay: 800, reset:true});
+sr.reveal(document.querySelector('.products__tabs'), {distance: '150%', origin: 'left', duration: 1200, reset:true});
+
+sr.reveal('.home__img', {interval: 900, duration: 3500, rotate: {x: 25, y: 55 }});
+ sr.reveal('.instructions__step', {interval: 500, duration: 1500, delay: 599, reset: true});
+ sr.reveal('.benefits__container', { scale: 0.3, duration: 3000, reset:true});
+ sr.reveal('.for-whom__item', {interval: 800, duration: 1500, reset:true});
+
+ sr.reveal('.products__card-container', {distance: '150%', origin: 'right', duration: 1200, reset:true});
+ sr.reveal('.faq__question', {interval: 900, duration: 1200, reset:true});
+
+
+ 
+};
